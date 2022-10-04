87f1fc571380bcdaf01cdb43ac44721dba5238e2
unknown <shakhawat-cse74>
2022-10-05 00:04:44 +0600

    pr54

---
+++ enroll/views.py
@@ -0,0 +1,15 @@
+from urllib import request
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+# Create your views here.
+
+def sign_up(request):
+    if request.method == 'POST':
+        fm = UserCreationForm(request.POST)
+        if fm.is_valid():
+            fm.save()
+            fm = UserCreationForm()
+    
+    else:
+        fm = UserCreationForm()
+    return render (request, 'enroll/signup.html', {'form':fm})
